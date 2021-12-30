efe5e802cdaa562bef44a1a24cd53449b8a90914
anongit <anongit@users.noreply.github.com>
2021-12-30 18:11:29 +0500

    Simplify UI settings

--- dragonspear_ui++/menu/common/ui_settings.lua
+++ dragonspear_ui++/menu/common/ui_settings.lua
@@ -1,32 +1,16 @@
 UIhelp = 0
-function selectedENum(text)
-	if QuicklootENum == "Ten" then
-		ENumCheck = 10
-	elseif QuicklootENum == "Six" then
-		ENumCheck = 6
-	elseif QuicklootENum == "Five" then
-		ENumCheck = 5
-	elseif QuicklootENum == "Four" then
-		ENumCheck = 4
-	elseif QuicklootENum == "Three" then
-		ENumCheck = 3
-	elseif QuicklootENum == "Two" then
-		ENumCheck = 2
-	end
-	if ENumCheck == tonumber(text) then
+
+function duiHighlightSelectedText(text, value, selected)
+	if value == selected then
 		return '^R' .. text .. '^-'
 	end
 	return text
 end
-function selectedQLPref(text)
-	if QuicklootPref ==  text then
-		return '^R' .. text .. '^-'
+
+function duiToggle(value, a, b)
+	if value == b then
+		return a
+	else
+		return b
 	end
-	return text
-end
-function selectedQLstartPref(text)
-	if QuicklootStartPref ==  text then
-		return '^R' .. text .. '^-'
-	end
-	return text
 end
